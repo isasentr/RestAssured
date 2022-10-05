c63cf96a0e3101bb665b49ff18af8edbddc68b89
isasentr <isasentr@gmail.com>
2022-10-05 21:49:07 +0300

    GoRest Campus

---
+++ src/test/java/Campus/Model/Country.java
@@ -0,0 +1,35 @@
+package Campus.Model;
+
+public class Country {
+   private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String code;
+   private String id;
+
+
+
+}
